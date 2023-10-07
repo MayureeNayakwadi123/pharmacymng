0595a548215b339689975ecbcd9f81d2ae4d6ef2
mayuree <mayureenayak1008@gmail.com>
2023-10-07 21:16:48 +0530

    Initial Commit

---
+++ src/main/java/com/itvedant/pharmacymng/PharmacymngApplication.java
@@ -0,0 +1,13 @@
+package com.itvedant.pharmacymng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacymngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacymngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itvedant/pharmacymng/PharmacymngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itvedant.pharmacymng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacymngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
